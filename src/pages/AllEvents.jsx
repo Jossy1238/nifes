eb68480e87a0cc76a2621400848874692c0cfcef
Jossy1238 <106344230+Jossy1238@users.noreply.github.com>
2023-12-15 08:43:46 +0400

    New Update

--- src/pages/AllEvents.jsx
+++ src/pages/AllEvents.jsx
@@ -43,7 +43,11 @@ function AllEvents(props) {
                 <div className='flex items-center justify-between'>
                     <div className='action__title'>
                         <h2 className='text-3xl font-500'>All Events</h2>
-                        <p className='opacity-5 text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eligendi?</p>
+                        <p className='opacity-5 text-lg'>
+                        Explore a world of exciting events, where memorable moments await you. 
+                        Discover and engage with a diverse range of activities that cater to your interests and passions. 
+                        Whether it's a cultural celebration, a thrilling performance, 
+                        or an enlightening seminar, our events bring people together for unforgettable experiences                            </p>
                     </div>
                     <NavLink to='/my-events/create'><button className='main__btn'>
                         <span>Create Event</span>
