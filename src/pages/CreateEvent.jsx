d86e9461ec06071206d4d52cd5f575673328ea0b
Jossy1238 <106344230+Jossy1238@users.noreply.github.com>
2023-12-19 23:47:29 +0400

    New Update

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -64,7 +64,7 @@ function CreateEvent(props) {
             <form onSubmit={handleSubmit} className='pt-3 create__event--form flex flex-col'>
                 <div className=''>
                     <h3 className='text-3xl font-500'>Create Event</h3>
-                    <p className='opacity-5 text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, temporibus!</p>
+                    <p className='opacity-5 text-lg'>Bring more event on board!!!</p>
                 </div>
                 {message.text && <div className={`border mt-2 w-full ${message.type=="error" ? "label__error":"label__success"}`}>{message.text}</div>}
 
